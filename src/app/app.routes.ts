9d54bab26588073a5b109d2b8489e62e7bdeed9c
mishoshup <202131799+mishoshup@users.noreply.github.com>
2025-09-17 15:15:15 +0800

    fix: add default route URL for landing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,18 +4,20 @@ export const routes: Routes = [
   {
     path: 'survey/:customer_id/project/:project_id',
     loadComponent: () =>
-      import('./survey-form/survey-form.component').then(
-        (m) => m.SurveyFormComponent
-      ),      
+      import('./survey-form/survey-form.component').then((m) => m.SurveyFormComponent),
   },
 
   // New internal staff evaluation survey
   {
     path: 'evaluation/:staff_id/project/:project_id/role/:role_type',
-    loadComponent: () => import('./evaluation-form/evaluation-form.component').then(
-      m => m.EvaluationFormComponent),
+    loadComponent: () =>
+      import('./evaluation-form/evaluation-form.component').then((m) => m.EvaluationFormComponent),
   },
 
-  //// keep this last as a catch-all or root route
-  // path: '', loadComponent: () => import('./survey-form/survey-form.component').then(m => m.SurveyFormComponent) }
+  // keep this last as a catch-all or root route
+  {
+    path: '',
+    loadComponent: () =>
+      import('./survey-form/survey-form.component').then((m) => m.SurveyFormComponent),
+  },
 ];
