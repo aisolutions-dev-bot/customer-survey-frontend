65d845f47bde258f4684579e3f046135e0371298
ai-solutions <dev@borneochemicalintl.com>
2025-10-13 20:32:41 +0800

    Update evaluation.service.ts
    change from dev to prod

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,15 +3,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'survey/:customer_id/project/:project_id',
-    renderMode: RenderMode.Server,
+    renderMode: RenderMode.Client,
   },
   {
     path: 'evaluation/:staff_id/project/:project_id/role/:role_type',
-    renderMode: RenderMode.Server,
+    renderMode: RenderMode.Client,
   },
   {
     path: 'evaluation/:staff_id/project/:project_id/role/:role_type/evaluator/:evaluator',
-    renderMode: RenderMode.Server,
+    renderMode: RenderMode.Client,
   },
   {
     path: '**',
